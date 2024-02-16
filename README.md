0abe4582435a2308c07d239d6ca8459047286516
Riviox <100956266+riviox@users.noreply.github.com>
2024-02-16 18:46:22 +0100

    add files

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# WaifuGenerator
- Waifu generator in html and js
+# Random Waifu Generator
+
+Welcome to the Random Waifu Generator! This web application allows you to generate random waifu images and interactions. Whether you're looking for a cute waifu, a playful neko, or something a bit more adventurous, our generator has you covered. You can toggle the NSFW option if you're feeling daring!
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+- [About](#about)
+- [Contact](#contact)
+
+## Features
+
+- Generate random SFW and NSFW waifu images.
+- Choose from various categories such as waifu, neko, shinobu, and more.
+- Toggle the NSFW option for a different experience.
+- Explore different interactions with your waifu.
+
+## Usage
+
+1. Open the application in your web browser.
+2. Toggle the NSFW option if desired.
+3. Choose a category from the dropdown menu.
+4. Click the "Generate" button to see your random waifu image.
+
+## About
+
+### About Waifu Generator
+
+The Waifu Generator is your source for randomly generated waifu images and interactions. Have fun exploring the world of waifus and discovering your next waifu companion.
+
+### Contact Us
+
+Feel free to contact us with any questions or feedback. We appreciate your input and strive to make the Waifu Generator a better experience for you.
+
+- Email: waifu.generator@example.com
+- Phone: +123 456 7890
+
+
---
+++ main.js
@@ -0,0 +1,66 @@
+const randomImage= document.querySelector(".random-image"),
+	  randomWaifu = document.querySelector(".random-waifu"),
+	  randomWaifuNsfw = document.querySelector(".random-waifu-nsfw"),
+	  formSelect = document.querySelector(".form-select"),
+	  formSelectNsfw = document.querySelector(".form-select-nsfw"),
+	  nsfwToggle = document.querySelector("#nsfwToggle"),
+	  imageWrapper = document.querySelector('.image-wrapper')
+document.querySelector(".sfw").style.display = "flex"
+document.querySelector(".nsfw").style.display = "none"
+
+formSelect.addEventListener('change', generateWaifu)
+formSelectNsfw.addEventListener('change', generateWaifu)
+
+let checkboxNsfw = document.querySelector('input[type=checkbox]');    
+
+nsfwToggle.addEventListener('change', ()=>{
+	if (checkboxNsfw.checked) {
+		document.querySelector(".nsfw").style.display = "flex"
+		document.querySelector(".sfw").style.display = "none"
+		generateWaifu()
+	} else {
+		document.querySelector(".sfw").style.display = "flex"
+		document.querySelector(".nsfw").style.display = "none"
+		generateWaifu()
+	}
+	
+})
+
+const loading = './load.svg'
+randomImage.src = loading
+
+randomWaifu.addEventListener("click",generateWaifu);
+randomWaifuNsfw.addEventListener("click",generateWaifu)
+
+function generateWaifu() {
+	randomImage.src = loading
+	if (document.querySelector(".sfw").style.display == "flex") {
+		fetchWaifu()
+	} else if (document.querySelector(".nsfw").style.display == "flex") {
+		fetchWaifuNsfw()
+	}
+
+
+}
+
+async function fetchWaifu() {
+	let category = formSelect.value;
+
+	await fetch(`https://api.waifu.pics/sfw/${category}`)
+		.then(response => response.json())
+		.then(quote =>  {
+			randomImage.src = quote.url
+	})
+}
+
+async function fetchWaifuNsfw() {
+	let category = formSelectNsfw.value;
+	await fetch(`https://api.waifu.pics/nsfw/${category}`)
+		.then(response => response.json())
+		.then(quote =>  {
+			randomImage.src = quote.url
+	})
+}
+
+
+generateWaifu()
