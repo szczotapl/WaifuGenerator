4c5bb40c5456386b7f63152f865dc8e474ee3330
Riviox <100956266+riviox@users.noreply.github.com>
2024-02-24 17:06:37 +0100

    Made electron app + fix css

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('https://riviox.is-a.dev/WaifuGenerator/');
+}
+
+app.whenReady().then(createWindow);
